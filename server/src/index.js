a1dda2a1542603499e48943b11a92cc0023f15ea
AlexandreNoguez <alexandrenoguez_rs@yahoo.com.br>
2022-07-27 20:49:05 -0300

    port config

--- server/src/index.js
+++ server/src/index.js
@@ -8,8 +8,10 @@ app.use(cors())
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
 
+const port = process.env.PORT || 3333;
+
 //Routes
 app.use('/api/user', userRoutes)
 app.use('/api/user', repositoriesRoutes)
 
-app.listen(3333)
+app.listen(port)
